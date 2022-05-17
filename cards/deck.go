a8e65806e64223aebf75e20b03aac3f9c1b43b1d
Fatih Koç <fatih.koc@ftechlabs.com>
2022-05-17 22:02:32 +0300

    newDeck function

--- cards/deck.go
+++ cards/deck.go
@@ -4,6 +4,17 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
