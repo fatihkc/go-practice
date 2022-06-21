159f2f85f09297cd7f44ada41e0615581672de62
Fatih Koç <fatih.koc@ftechlabs.com>
2022-06-21 23:17:42 +0300

    structs

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName  string
+}
+
 func main() {
-	cards := newDeck()
-	cards.shuffle()
-	cards.print()
+	var alex person
+
+	alex.firstName = "Alex"
+	alex.lastName = "Anderson"
+
+	fmt.Println(alex)
+	fmt.Printf("%+v", alex)
 }
