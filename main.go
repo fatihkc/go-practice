52b275bfebcbd945d49f27df8dc7e8025d4ff3e3
Fatih Koç <mail@fatihkoc.net>
2022-06-23 22:38:34 +0300

    map

--- main.go
+++ main.go
@@ -2,35 +2,18 @@ package main
 
 import "fmt"
 
-type contactInfo struct {
-	email   string
-	zipCode int
-}
-
-type person struct {
-	firstName string
-	lastName  string
-	contact   contactInfo
-}
-
 func main() {
-	jim := person{
-		firstName: "Jim",
-		lastName:  "Party",
-		contact: contactInfo{
-			email:   "jim@gmail.com",
-			zipCode: 94000,
-		},
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#00ff00",
+		"blue":  "#0000ff",
 	}
 
-	jim.updateName("jimmy")
-	jim.print()
+	printMap(colors)
 }
 
-func (pointerToPerson *person) updateName(newFirstName string) {
-	(*pointerToPerson).firstName = newFirstName
-}
-
-func (p person) print() {
-	fmt.Printf("%+v", p)
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
