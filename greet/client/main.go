e9c5785ce94d5a229ccbfb0440583356b8ec8f35
Fatih Koç <mail@fatihkoc.net>
2022-07-25 21:26:09 +0300

    ssl

--- greet/client/main.go
+++ greet/client/main.go
@@ -2,17 +2,30 @@ package main
 
 import (
 	"log"
-	"time"
 
 	pb "github.com/fatihkc/go-practice/greet/proto"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/credentials"
 )
 
 var addr string = "localhost:50051"
 
 func main() {
-	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	tls := true
+	opts := []grpc.DialOption{}
+
+	if tls {
+		certFile := "ssl/ca.crt"
+		creds, err := credentials.NewClientTLSFromFile(certFile, "")
+
+		if err != nil {
+			log.Fatalf("Error while loading CA trust certificate")
+		}
+
+		opts = append(opts, grpc.WithTransportCredentials(creds))
+	}
+
+	conn, err := grpc.Dial(addr, opts...)
 
 	if err != nil {
 		log.Fatalf("failed to dial: %v\n", err)
@@ -21,9 +34,9 @@ func main() {
 
 	c := pb.NewGreetServiceClient(conn)
 
-	// doGreet(c)
+	doGreet(c)
 	// doGreetManyTimes(c)
 	// doLongGreet(c)
 	// doGreetEveryone(c)
-	doGreetWithDeadline(c, 5*time.Second)
+	// doGreetWithDeadline(c, 5*time.Second)
 }
